eda692133094305cce9892a79d197ef9847b1887
Hen Shalom <xdrakolax@gmail.com>
2017-07-23 20:54:06 +0300

    done

---
+++ README.md
@@ -0,0 +1,23 @@
+# material-multi-search
+
+## Introduction
+
+> this npm package let you use a multi search component build with the help of material ui 
+
+>what is multi-search ?        multi search is a component that let the customer pick and unpick a few item to a list . this easy to use component expose to a function that being call every time
+
+## Code Samples
+
+> You've gotten their attention in the introduction, now show a few code examples. So they get a visualization and as a bonus, make them copy/paste friendly.
+
+## Installation
+
+```sh
+$ npm install material-multi-search
+```
+
+
+
+##TODO's:
+
+(if you have any suggestion please go the github link)
--- src/material-multi-search.js
+++ src/material-multi-search.js
@@ -1,40 +1,115 @@
 import React, { Component } from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
-import FlatButton from 'material-ui/FlatButton'
+import equal from 'deep-equal'
+import RaisedButton from 'material-ui/RaisedButton'
+
 
 class MultiSearch extends Component {
-    constructor(props,context) {
-            super(props,context)
+    constructor(props, context) {
+        super(props, context)
+        console.log(props.data)
         this.state = {
-            dataSource: [],
+            hinText: props.hinText,
+            maxResult: props.maxResult,
+            inputData: props.data,
+            data: this.dataDicider(props.data, props.converter),
+            selectedItem: [],
+            selectedItemConvereted: [],
+            maxItems: props.maxItems,
+            onItemChange: props.onItemChange,
         };
-        this.handleUpdateInput = this.handleUpdateInput.bind(this)
+        this.handleItemChange = this.handleItemChange.bind(this);
     }
 
-    handleUpdateInput(value) {
-        this.setState({
-            dataSource: [
-                value,
-                value + value,
-                value + value + value,
-            ],
-        });
-    };
+    dataDicider(data, converter) {
+        if (typeof (data[0]) === "object") {
+            console.log("this is an object list")
+            return this.mangedData(data, converter)
+        } else {
+            return data
+        }
+    }
 
+    mangedData(data, converter) {
+        let convertedData = [];
+        for (let d in data) {
+            convertedData.push(converter(data[d]));
+        }
+        return convertedData;
+    }
+
+
+    handleItemChange(value, index) {
+        let item = this.state.inputData[index];
+        if (this.state.selectedItem.length < this.state.maxItems) {
+            if (!this.checkInList(this.state.selectedItem, item)) {
+                this.setState((prevState) => {
+                    let newItems = prevState.selectedItem;
+                    let convertedItem = prevState.selectedItemConvereted
+                    newItems.push(item);
+                    convertedItem.push(this.state.data[index])
+                    return { selectedItem: newItems, selectedItemConvereted: convertedItem };
+                });
+            }
+        }
+        this.state.onItemChange(this.state.selectedItem);
+    }
+
+    checkInList(selectedItem, item) {
+        for (let i = 0; i < selectedItem.length; i++) {
+            if (equal(item, selectedItem[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    handleListClick(index) {
+        console.log(index)
+        this.setState((prevState) => {
+            return { selectedItem: this.removeElmentAt(prevState.selectedItem, index), selectedItemConvereted: this.removeElmentAt(prevState.selectedItemConvereted, index) }
+        })
+    }
+    removeElmentAt(arr, index) {
+        let len = arr.length
+        let newArr=[];
+        for (let i = 0; i < len; i++) {
+            if (i !== index) {
+                newArr.push(arr[i])
+            }
+        }
+        return newArr
+    }
 
     render() {
+
         return (
-            <div>
-                <p>hello</p>
-                    <FlatButton label="hen the man"/>
+
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                {this.state.selectedItemConvereted.map((item, index) => {
+                    return <RaisedButton key={index} style={{ margin: 5 }} secondary={true} label={item} onTouchTap={this.handleListClick.bind(this, index)} />
+                })}
+                <AutoComplete
+                    floatingLabelText={this.state.hinText}
+                    filter={AutoComplete.caseInsensitiveFilter}
+                    dataSource={this.state.data}
+                    maxSearchResults={this.state.maxResult}
+                    onNewRequest={this.handleItemChange}
+                    fullWidth={true}
+
+                />
             </div>
         )
-
     }
-
-
-
 }
+
+MultiSearch.defaultProps = {
+    data: [],
+    maxItems: 5,
+    onItemChange: () => { },
+
+
+};
 
 
 export default MultiSearch;
